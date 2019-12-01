047bdd43478c2af601e8d1e2e84cc441289aaae9
Irthir <romain.schlotter@gmail.com>
2019-12-01 13:07:34 +0100

    Envoie d'un "Bonjour" du serveur vers le client.

--- Test_Sockets/ApplicationClientTestSockets/main.c
+++ Test_Sockets/ApplicationClientTestSockets/main.c
@@ -32,7 +32,9 @@ int main(void)
 
     SOCKET sock;
     SOCKADDR_IN sin;
+    char buffer[32] = "";
 
+    /* Si les sockets Windows fonctionnent */
     if(!erreur)
     {
         /* Création de la socket */
@@ -43,19 +45,31 @@ int main(void)
         sin.sin_family = AF_INET;
         sin.sin_port = htons(PORT);
 
-        /* Si le client arrive à se connecter */
+        /* Si l'on a réussi à se connecter */
         if(connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR)
-            printf("Connexion à %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
-        else
-            printf("Impossible de se connecter\n");
+        {
+            printf("Connection à %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
 
-        /* On ferme la socket précédemment ouverte */
+            /* Si l'on reçoit des informations : on les affiche à l'écran */
+            if(recv(sock, buffer, 32, 0) != SOCKET_ERROR)
+                printf("Recu : %s\n", buffer);
+        }
+        /* sinon, on affiche "Impossible de se connecter" */
+        else
+        {
+            printf("Impossible de se connecter\n");
+        }
+
+        /* On ferme la socket */
         closesocket(sock);
 
         #if defined (WIN32)
             WSACleanup();
         #endif
     }
+
+    /* On attend que l'utilisateur tape sur une touche, puis on ferme */
+    getchar();
 
     return EXIT_SUCCESS;
 }
